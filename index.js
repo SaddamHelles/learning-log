e949802959e1dc2518363d8fc1bfdc8b62b15230
Saddam Hilles <sdm.hilles@gmail.com>
2022-08-07 12:28:06 +0300

    print full name

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log('Welcome dr.');
 const firstName = 'Saddam';
 const lastName = 'Helles';
-const fullName = `Your name is: ${firstName} ${lastName}`;
+const fullName = `Your full name: ${firstName} ${lastName}`;
 console.log(fullName)
