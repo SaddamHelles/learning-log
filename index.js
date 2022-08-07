151133c4d23922e4236d31de1ecb9e324ab19e73
Saddam Hilles <sdm.hilles@gmail.com>
2022-08-07 14:48:01 +0300

    userInfo commit

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ console.log('Welcome dr.');
 const firstName = 'Saddam';
 const lastName = 'Helles';
 const age = 31;
-const fullName = `Your full name: ${firstName} ${lastName}, your age: ${age}`;
-console.log(fullName)
+const infoUser = `Your full name: ${firstName} ${lastName}, your age: ${age}`;
+console.log(infoUser)
 const fetchedDatat = '';
